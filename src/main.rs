1a25179e27082489e18ca064687a9d9e309efdf9
Avery Larsen <averylarsen@mailbox.org>
2023-12-20 12:37:33 -0800

    Add Rust project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn message() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    println!("{}", message());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_message() {
+        assert_eq!(message(), "Hello, world!")
+    }
+}
