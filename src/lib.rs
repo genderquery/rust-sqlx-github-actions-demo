89daedfdf236b7f147922c6f7c94f4a80a4f2b09
Avery Larsen <averylarsen@mailbox.org>
2023-12-20 19:41:57 -0800

    Refactor integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
+use sqlx::{Pool, Postgres};
+
 pub fn message() -> &'static str {
     "Hello, world!"
+}
+
+pub async fn get_message_from_db(pool: Pool<Postgres>) -> Result<String, sqlx::Error> {
+    let row = sqlx::query!("select * from messages")
+        .fetch_one(&pool)
+        .await?;
+    Ok(row.message)
 }
 
 #[cfg(test)]
--- tests/postgres-demo.rs
+++ tests/postgres-demo.rs
@@ -1,12 +1,9 @@
+use github_actions_demo::get_message_from_db;
 use sqlx::PgPool;
 
 #[sqlx::test(fixtures("messages"))]
 async fn test_message(pool: PgPool) -> sqlx::Result<()> {
-    let row = sqlx::query!("select * from messages")
-        .fetch_one(&pool)
-        .await?;
-
-    assert_eq!(row.message, "Hello, world!");
-
+    let message = get_message_from_db(pool).await?;
+    assert_eq!(message, "Hello, world!");
     Ok(())
 }
